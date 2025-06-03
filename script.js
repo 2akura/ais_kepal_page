30efed466b4d465814eee2f75b0fb816de1f2af0
花 <168328470+2akura@users.noreply.github.com>
2025-06-03 12:29:26 +0800

    Update script.js
    const toppings = document.querySelectorAll('.feature-checkbox:checked');

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ document.querySelector('.submit').addEventListener('click', function(event) {
     }
   
     // Toppings
-    const toppings = document.querySelectorAll('input[name="feature"]:checked');
+    const toppings = document.querySelectorAll('.feature-checkbox:checked');
+
     toppings.forEach(t => {
       const item = JSON.parse(t.value);
       total += item.price;
